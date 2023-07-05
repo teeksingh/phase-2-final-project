9127f9657b565e4305556fa24dde44f8668acea5
Prateek <teeksingh@gmail.com>
2023-07-04 21:45:02 -0500

    Added NavBar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 
 function NavBar() {
     
-    
     return(
-
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/RandomRecipe">Generate Random Recipe</Link>
+            <Link to="/Search">Search</Link>
+            
+        </nav>
     )
 }
 
