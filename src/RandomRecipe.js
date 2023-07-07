f8286082584b6ce4c0e6c48c51f6e989ebe5f92d
Prateek <teeksingh@gmail.com>
2023-07-06 22:11:02 -0500

    attempting to resolve ESLint Parsing error Unexpected token by installing a new package

--- src/RandomRecipe.js
+++ src/RandomRecipe.js
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from "react";
 function RandomRecipe(){
 
     return (
-        
+      <div>
+        <h2>test2</h2>
+      </div>  
     )
 }
 export default RandomRecipe
--- src/Search.js
+++ src/Search.js
@@ -4,7 +4,9 @@ import React, { useState, useEffect } from "react";
 function Search() {
 
     return(
-
+<div>
+    <h1>test</h1>
+</div>
     )
 }
 export default Search
